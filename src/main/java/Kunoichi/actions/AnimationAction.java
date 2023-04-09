4bfaab39d7980b3a0075aa1ec5a4db47b550b5c3
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-09 16:45:39 -0300

    Add Counter - Damage

---
+++ src/main/java/Kunoichi/actions/AnimationAction.java
@@ -0,0 +1,31 @@
+package Kunoichi.actions;
+
+import Kunoichi.TheKunoichi;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+
+public class AnimationAction extends AbstractGameAction {
+    public enum Animation {
+        ATTACK,
+        BLOCK,
+        HAPPY,
+        HURT
+    }
+
+    private Animation anim;
+
+    public AnimationAction(Animation anim) {
+        this.anim = anim;
+    }
+
+    @Override
+    public void update() {
+        if (AbstractDungeon.player instanceof TheKunoichi) {
+            ((TheKunoichi) AbstractDungeon.player).playAnimation(anim.name().toLowerCase());
+            if (anim == Animation.ATTACK) {
+                AbstractDungeon.player.useFastAttackAnimation();
+            }
+        }
+        this.isDone = true;
+    }
+}
---
+++ src/main/java/Kunoichi/powers/CounterDamagePower.java
@@ -0,0 +1,44 @@
+package Kunoichi.powers;
+
+import Kunoichi.actions.AnimationAction;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.actions.common.LoseHPAction;
+import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.core.AbstractCreature;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.rooms.AbstractRoom;
+
+import static Kunoichi.KunoichiMod.makeID;
+
+public class CounterDamagePower extends AbstractEasyPower {
+    public static String POWER_ID = makeID(CounterDamagePower.class.getSimpleName());
+    public static PowerStrings strings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
+    public static final String NAME = strings.NAME;
+    public static final String[] DESCRIPTIONS = strings.DESCRIPTIONS;
+
+    public CounterDamagePower(AbstractCreature owner, int amount) {
+        super(NAME, PowerType.BUFF, true, owner, amount);
+        this.ID = POWER_ID;
+        this.loadRegion("noPain");
+    }
+
+    @Override
+    public int onAttacked(DamageInfo info, int damageAmount) {
+        if (info.type != DamageInfo.DamageType.THORNS && info.type != DamageInfo.DamageType.HP_LOSS && info.owner != null && info.owner != this.owner) {
+            this.flash();
+            addToTop(new RemoveSpecificPowerAction(owner, owner, this));
+            addToTop(new DamageAction(info.owner, new DamageInfo(this.owner, this.amount, DamageInfo.DamageType.THORNS), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL));
+            addToTop(new AnimationAction(AnimationAction.Animation.ATTACK));
+        }
+        return damageAmount;
+    }
+
+    @Override
+    public void updateDescription() {
+        this.description = DESCRIPTIONS[0] + this.amount + DESCRIPTIONS[1];
+    }
+}
