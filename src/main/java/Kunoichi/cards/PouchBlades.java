8d7f85235dd85e00781527ffe3bacda9507b5a3d
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-09 19:50:39 -0300

    Add Pouch Blades

---
+++ src/main/java/Kunoichi/cards/PouchBlades.java
@@ -0,0 +1,50 @@
+package Kunoichi.cards;
+
+import Kunoichi.cards.abstracts.AbstractEasyCard;
+import Kunoichi.util.CardArtRoller;
+import Kunoichi.vfx.ColoredFlyingDaggerEffect;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.colorless.SecretWeapon;
+import com.megacrit.cardcrawl.cards.green.Strike_Green;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.ThrowDaggerEffect;
+import javafx.scene.paint.Color;
+
+import static Kunoichi.KunoichiMod.makeID;
+
+public class PouchBlades extends AbstractEasyCard {
+    public final static String ID = makeID(PouchBlades.class.getSimpleName());
+
+    public PouchBlades() {
+        super(ID, 2, CardType.ATTACK, CardRarity.BASIC, CardTarget.ENEMY);
+        baseDamage = damage = 4;
+        baseMagicNumber = magicNumber = 3;
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0 ; i < magicNumber ; i++) {
+            if (m != null) {
+                addToBot(new VFXAction(new ThrowDaggerEffect(m.hb.cX, m.hb.cY)));
+            }
+            addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.NONE, true));
+        }
+    }
+
+    public void upp() {
+        upgradeBaseCost(1);
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, 0.5f, 0.5f, 0.3f, 0.5f, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return SecretWeapon.ID;
+    }
+}
