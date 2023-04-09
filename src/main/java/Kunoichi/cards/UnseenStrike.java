b233b750b0333d970853c945ec2b5a4675c042c9
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-09 16:46:23 -0300

    Add Unseen Strike

---
+++ src/main/java/Kunoichi/cards/UnseenStrike.java
@@ -0,0 +1,57 @@
+package Kunoichi.cards;
+
+import Kunoichi.actions.DoIfAction;
+import Kunoichi.cards.abstracts.AbstractEasyCard;
+import Kunoichi.util.CardArtRoller;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.green.MasterfulStab;
+import com.megacrit.cardcrawl.cards.green.QuickSlash;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Kunoichi.KunoichiMod.makeID;
+
+public class UnseenStrike extends AbstractEasyCard {
+    public final static String ID = makeID(UnseenStrike.class.getSimpleName());
+
+    public UnseenStrike() {
+        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = damage = 7;
+        tags.add(CardTags.STRIKE);
+    }
+
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL);
+        addToBot(new DoIfAction(() -> isNotAttacking(m), () -> dmgTop(m, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL)));
+    }
+
+    public void upp() {
+        upgradeDamage(3);
+    }
+
+    private boolean isNotAttacking(AbstractMonster m) {
+        return m == null || m.getIntentBaseDmg() < 0;
+    }
+
+    public void triggerOnGlowCheck() {
+        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
+            if (isNotAttacking(m)) {
+                this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+                return;
+            }
+        }
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, 0.5f, 0.5f, 0.2f, 0.5f, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return MasterfulStab.ID;
+    }
+}
