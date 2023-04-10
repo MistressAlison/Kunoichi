d010e4a88be269dc8a139bc474b78ab5a4b85930
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-10 09:09:35 -0300

    Add Go for the Heart

---
+++ src/main/java/Kunoichi/cards/GoForTheHeart.java
@@ -0,0 +1,65 @@
+package Kunoichi.cards;
+
+import Kunoichi.actions.ApplyPowerActionWithFollowup;
+import Kunoichi.actions.DoIfAction;
+import Kunoichi.cards.abstracts.AbstractEasyCard;
+import Kunoichi.powers.BleedPower;
+import Kunoichi.util.CardArtRoller;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.green.SuckerPunch;
+import com.megacrit.cardcrawl.cards.red.BloodForBlood;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.LoseStrengthPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+import com.megacrit.cardcrawl.powers.VulnerablePower;
+
+import java.util.Iterator;
+
+import static Kunoichi.KunoichiMod.makeID;
+
+public class GoForTheHeart extends AbstractEasyCard {
+    public final static String ID = makeID(GoForTheHeart.class.getSimpleName());
+
+    public GoForTheHeart() {
+        super(ID, 0, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = damage = 6;
+        baseMagicNumber = magicNumber = 6;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL);
+        addToBot(new DoIfAction(() -> m.hasPower(VulnerablePower.POWER_ID), () -> addToTop(new ApplyPowerAction(m, p, new BleedPower(m, p, magicNumber)))));
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(2);
+        upgradeMagicNumber(2);
+    }
+
+    public void triggerOnGlowCheck() {
+        this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
+        for (AbstractMonster m : AbstractDungeon.getCurrRoom().monsters.monsters) {
+            if (!m.isDeadOrEscaped() && m.hasPower(VulnerablePower.POWER_ID)) {
+                this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
+                break;
+            }
+        }
+
+    }// 46
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, 0.5f, 0.5f, 0.25f, 0.5f, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return BloodForBlood.ID;
+    }
+}
