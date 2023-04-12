5b3b7a6be9f7a7d267570965ed8d382b684b3f30
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-12 02:53:49 -0300

    Add Arte - Pangolin Form

---
+++ src/main/java/Kunoichi/cards/ArtePangolin.java
@@ -0,0 +1,55 @@
+package Kunoichi.cards;
+
+import Kunoichi.actions.ApplyPowerActionWithFollowup;
+import Kunoichi.cards.abstracts.AbstractEasyCard;
+import Kunoichi.util.CardArtRoller;
+import Kunoichi.util.Wiz;
+import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
+import com.megacrit.cardcrawl.actions.common.GainBlockAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.red.Berserk;
+import com.megacrit.cardcrawl.cards.red.Sentinel;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.LoseStrengthPower;
+import com.megacrit.cardcrawl.powers.StrengthPower;
+
+import static Kunoichi.KunoichiMod.makeID;
+
+public class ArtePangolin extends AbstractEasyCard {
+    public final static String ID = makeID(ArtePangolin.class.getSimpleName());
+
+    public ArtePangolin() {
+        super(ID, -2, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.ALL_ENEMY);
+        baseMagicNumber = magicNumber = 2;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {}
+
+    @Override
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        this.cantUseMessage = cardStrings.EXTENDED_DESCRIPTION[0];
+        return false;
+    }
+
+    @Override
+    public void triggerOnOtherCardPlayed(AbstractCard c) {
+        addToBot(new GainBlockAction(Wiz.adp(), magicNumber));
+    }
+
+    @Override
+    public void upp() {
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, 0.5f, 0.5f, 0.25f, 0.5f, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Sentinel.ID;
+    }
+}
