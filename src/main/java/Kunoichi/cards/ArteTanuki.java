77ebac6fccce0062ee238e1634c448e86273ed36
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-12 03:11:15 -0300

    Add Arte - Tanuki Form

---
+++ src/main/java/Kunoichi/cards/ArteTanuki.java
@@ -0,0 +1,54 @@
+package Kunoichi.cards;
+
+import Kunoichi.cards.abstracts.AbstractEasyCard;
+import Kunoichi.patches.OnUseEnergyPatches;
+import Kunoichi.util.CardArtRoller;
+import Kunoichi.util.Wiz;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.purple.JustLucky;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Kunoichi.KunoichiMod.makeID;
+
+public class ArteTanuki extends AbstractEasyCard implements OnUseEnergyPatches.OnUseEnergyObject {
+    public final static String ID = makeID(ArteTanuki.class.getSimpleName());
+
+    public ArteTanuki() {
+        super(ID, -2, CardType.SKILL, CardRarity.RARE, CardTarget.NONE);
+        isEthereal = true;
+        baseMagicNumber = magicNumber = 1;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {}
+
+    @Override
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        this.cantUseMessage = cardStrings.EXTENDED_DESCRIPTION[0];
+        return false;
+    }
+
+    @Override
+    public void upp() {
+        isEthereal = false;
+        uDesc();
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, 0.5f, 0.5f, 0.25f, 0.5f, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return JustLucky.ID;
+    }
+
+    @Override
+    public void onUseEnergy(int amount) {
+        if (amount > 0 && Wiz.adp().hand.contains(this)) {
+            addToBot(new DrawCardAction(magicNumber));
+        }
+    }
+}
