1327bffc18dd829854a7ec837e44dff45a522aa2
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-12 03:15:39 -0300

    Add Arte - Dragon Form

---
+++ src/main/java/Kunoichi/cards/ArteDragon.java
@@ -0,0 +1,52 @@
+package Kunoichi.cards;
+
+import Kunoichi.cards.abstracts.AbstractEasyCard;
+import Kunoichi.util.CardArtRoller;
+import Kunoichi.util.Wiz;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAllEnemiesAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.red.FireBreathing;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Kunoichi.KunoichiMod.makeID;
+
+public class ArteDragon extends AbstractEasyCard {
+    public final static String ID = makeID(ArteDragon.class.getSimpleName());
+
+    public ArteDragon() {
+        super(ID, -2, CardType.SKILL, CardRarity.RARE, CardTarget.NONE);
+        baseMagicNumber = magicNumber = 3;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {}
+
+    @Override
+    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
+        this.cantUseMessage = cardStrings.EXTENDED_DESCRIPTION[0];
+        return false;
+    }
+
+    @Override
+    public void triggerOnOtherCardPlayed(AbstractCard c) {
+        addToBot(new DamageAllEnemiesAction(Wiz.adp(), DamageInfo.createDamageMatrix(magicNumber, true), DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.FIRE, true));
+    }
+
+    @Override
+    public void upp() {
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, 0.5f, 0.5f, 0.25f, 0.5f, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return FireBreathing.ID;
+    }
+}
