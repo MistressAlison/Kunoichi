fa1704fde7f56eb79eda0b02ce844288c79678ae
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-14 16:49:16 -0300

    Add Clairvoyance

---
+++ src/main/java/Kunoichi/cards/Clairvoyance.java
@@ -0,0 +1,41 @@
+package Kunoichi.cards;
+
+import Kunoichi.actions.SurveyAction;
+import Kunoichi.cards.abstracts.AbstractEasyCard;
+import Kunoichi.util.CardArtRoller;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.cards.colorless.Enlightenment;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Kunoichi.KunoichiMod.makeID;
+
+public class Clairvoyance extends AbstractEasyCard {
+    public final static String ID = makeID(Clairvoyance.class.getSimpleName());
+
+    public Clairvoyance() {
+        super(ID, 1, CardType.SKILL, CardRarity.UNCOMMON, CardTarget.NONE);
+        baseMagicNumber = magicNumber = 2;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        addToBot(new DrawCardAction(magicNumber));
+        addToBot(new SurveyAction(magicNumber));
+    }
+
+    @Override
+    public void upp() {
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, 0.5f, 0.5f, 0.25f, 0.5f, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Enlightenment.ID;
+    }
+}
