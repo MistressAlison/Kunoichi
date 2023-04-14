bb715b1b851b1e97ffd381a4de7337bef9c98d6a
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-14 16:19:53 -0300

    Change Ensnaring Strike

---
+++ src/main/java/Kunoichi/cards/EnsnaringStrike.java
@@ -0,0 +1,45 @@
+package Kunoichi.cards;
+
+import Kunoichi.cards.abstracts.AbstractEasyCard;
+import Kunoichi.util.CardArtRoller;
+import Kunoichi.util.Wiz;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.colorless.DarkShackles;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
+
+import static Kunoichi.KunoichiMod.makeID;
+
+public class EnsnaringStrike extends AbstractEasyCard {
+    public final static String ID = makeID(EnsnaringStrike.class.getSimpleName());
+
+    public EnsnaringStrike() {
+        super(ID, 0, CardType.ATTACK, CardRarity.UNCOMMON, CardTarget.ENEMY);
+        baseDamage = damage = 6;
+        baseMagicNumber = magicNumber = 1;
+        tags.add(CardTags.STRIKE);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.SLASH_DIAGONAL);
+        Wiz.applyToEnemy(m, new WeakPower(m, magicNumber, false));
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(2);
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, 0.5f, 0.5f, 0.25f, 0.5f, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return DarkShackles.ID;
+    }
+}
