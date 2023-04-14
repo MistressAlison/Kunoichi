9f401ee8f49b813615f72a6202c82104df0cf703
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-14 00:19:29 -0300

    Use helper methods

--- src/main/java/Kunoichi/cards/UnseenStrike.java
+++ src/main/java/Kunoichi/cards/UnseenStrike.java
@@ -3,12 +3,11 @@ package Kunoichi.cards;
 import Kunoichi.actions.DoIfAction;
 import Kunoichi.cards.abstracts.AbstractEasyCard;
 import Kunoichi.util.CardArtRoller;
+import Kunoichi.util.Wiz;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.green.MasterfulStab;
-import com.megacrit.cardcrawl.cards.green.QuickSlash;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 
 import static Kunoichi.KunoichiMod.makeID;
@@ -25,7 +24,7 @@ public class UnseenStrike extends AbstractEasyCard {
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
         dmg(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL);
-        addToBot(new DoIfAction(() -> isNotAttacking(m), () -> dmgTop(m, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL)));
+        addToBot(new DoIfAction(() -> !Wiz.isAttacking(m), () -> dmgTop(m, AbstractGameAction.AttackEffect.SLASH_HORIZONTAL)));
     }
 
     @Override
@@ -33,18 +32,11 @@ public class UnseenStrike extends AbstractEasyCard {
         upgradeDamage(3);
     }
 
-    private boolean isNotAttacking(AbstractMonster m) {
-        return m == null || m.getIntentBaseDmg() < 0;
-    }
-
     @Override
     public void triggerOnGlowCheck() {
         this.glowColor = AbstractCard.BLUE_BORDER_GLOW_COLOR.cpy();
-        for (AbstractMonster m : AbstractDungeon.getMonsters().monsters) {
-            if (!m.isDeadOrEscaped() && isNotAttacking(m)) {
-                this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
-                return;
-            }
+        if (Wiz.anyMonsterNotAttacking()) {
+            this.glowColor = AbstractCard.GOLD_BORDER_GLOW_COLOR.cpy();
         }
     }
 
