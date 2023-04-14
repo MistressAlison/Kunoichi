5379ca147dbd8c18235aeb8345e41e63e44bd195
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-14 12:18:06 -0300

    Add Tromp

---
+++ src/main/java/Kunoichi/cards/Tromp.java
@@ -0,0 +1,45 @@
+package Kunoichi.cards;
+
+import Kunoichi.cards.abstracts.AbstractEasyCard;
+import Kunoichi.util.CardArtRoller;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.actions.common.DamageAction;
+import com.megacrit.cardcrawl.cards.DamageInfo;
+import com.megacrit.cardcrawl.cards.purple.Rushdown;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+
+import static Kunoichi.KunoichiMod.makeID;
+
+public class Tromp extends AbstractEasyCard {
+    public final static String ID = makeID(Tromp.class.getSimpleName());
+
+    public Tromp() {
+        super(ID, 2, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = damage = 6;
+        baseMagicNumber = magicNumber = 3;
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        for (int i = 0 ; i < magicNumber ; i++) {
+            addToBot(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.BLUNT_HEAVY));
+        }
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(2);
+        //upgradeMagicNumber(1);
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, 0.5f, 0.5f, 0.3f, 0.5f, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return Rushdown.ID;
+    }
+}
