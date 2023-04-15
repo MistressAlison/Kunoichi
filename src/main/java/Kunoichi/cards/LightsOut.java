985bbe2c437bb6783721665a7e2157dbbaecee13
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-15 16:27:50 -0300

    Rework Lights Out

--- src/main/java/Kunoichi/cards/LightsOut.java
+++ src/main/java/Kunoichi/cards/LightsOut.java
@@ -1,14 +1,19 @@
 package Kunoichi.cards;
 
 import Kunoichi.actions.DamageFollowupAction;
+import Kunoichi.actions.OpenerAction;
 import Kunoichi.cards.abstracts.AbstractEasyCard;
 import Kunoichi.util.CardArtRoller;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.GainEnergyAction;
+import com.megacrit.cardcrawl.actions.animations.VFXAction;
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.actions.utility.WaitAction;
+import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.purple.FearNoEvil;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.WeightyImpactEffect;
 
 import static Kunoichi.KunoichiMod.makeID;
 
@@ -17,25 +22,30 @@ public class LightsOut extends AbstractEasyCard {
 
     public LightsOut() {
         super(ID, 3, CardType.ATTACK, CardRarity.RARE, CardTarget.ENEMY);
-        baseDamage = damage = 30;
-        baseMagicNumber = magicNumber = 3;
+        baseDamage = damage = 26;
+        baseMagicNumber = magicNumber = 2;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
+        if (m != null) {
+            this.addToBot(new VFXAction(new WeightyImpactEffect(m.hb.cX, m.hb.cY)));
+            this.addToBot(new WaitAction(0.8F));
+        }
         addToBot(new DamageFollowupAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.NONE, t -> {
            if (t.isDying || t.currentHealth <= 0) {
-               returnToHand = true;
-               addToTop(new GainEnergyAction(magicNumber));
-           } else {
-               returnToHand = false;
+               addToTop(new DrawCardAction(magicNumber, new OpenerAction(() -> {
+                   for (AbstractCard c : DrawCardAction.drawnCards) {
+                       c.setCostForTurn(0);
+                   }
+               })));
            }
         }));
     }
 
     @Override
     public void upp() {
-        upgradeDamage(10);
+        upgradeDamage(8);
     }
 
     @Override
