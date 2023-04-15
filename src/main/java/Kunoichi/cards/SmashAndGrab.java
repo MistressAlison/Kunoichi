95687c7eb578f7976da3e0b45941116f759bd14d
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-14 23:51:29 -0300

    Add Smash and Grab

---
+++ src/main/java/Kunoichi/cards/SmashAndGrab.java
@@ -0,0 +1,47 @@
+package Kunoichi.cards;
+
+import Kunoichi.actions.SurveyAction;
+import Kunoichi.cards.abstracts.AbstractEasyCard;
+import Kunoichi.util.CardArtRoller;
+import Kunoichi.util.Wiz;
+import com.megacrit.cardcrawl.actions.AbstractGameAction;
+import com.megacrit.cardcrawl.cards.colorless.DarkShackles;
+import com.megacrit.cardcrawl.cards.red.PommelStrike;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.powers.WeakPower;
+
+import static Kunoichi.KunoichiMod.makeID;
+
+public class SmashAndGrab extends AbstractEasyCard {
+    public final static String ID = makeID(SmashAndGrab.class.getSimpleName());
+
+    public SmashAndGrab() {
+        super(ID, 1, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY);
+        baseDamage = damage = 9;
+        baseMagicNumber = magicNumber = 2;
+        tags.add(CardTags.STRIKE);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        dmg(m, AbstractGameAction.AttackEffect.BLUNT_LIGHT);
+        addToBot(new SurveyAction(magicNumber));
+    }
+
+    @Override
+    public void upp() {
+        upgradeDamage(2);
+        upgradeMagicNumber(1);
+    }
+
+    @Override
+    public CardArtRoller.ReskinInfo reskinInfo(String ID) {
+        return new CardArtRoller.ReskinInfo(ID, 0.5f, 0.5f, 0.25f, 0.5f, false);
+    }
+
+    @Override
+    public String cardArtCopy() {
+        return PommelStrike.ID;
+    }
+}
