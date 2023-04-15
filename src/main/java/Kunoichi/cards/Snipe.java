acd48d489e4ad02d6b2623b332b7ab17f7a5c6e1
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-15 12:11:44 -0300

    Rework Snipe

--- src/main/java/Kunoichi/cards/Snipe.java
+++ src/main/java/Kunoichi/cards/Snipe.java
@@ -1,14 +1,12 @@
 package Kunoichi.cards;
 
 import Kunoichi.cards.abstracts.AbstractEasyCard;
-import Kunoichi.powers.ExposedPower;
 import Kunoichi.util.CardArtRoller;
 import Kunoichi.util.Wiz;
 import com.evacipated.cardcrawl.mod.stslib.actions.common.SelectCardsInHandAction;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
-import com.megacrit.cardcrawl.actions.common.ApplyPowerAction;
 import com.megacrit.cardcrawl.actions.common.DamageAction;
-import com.megacrit.cardcrawl.actions.common.DiscardSpecificCardAction;
+import com.megacrit.cardcrawl.actions.common.ExhaustSpecificCardAction;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.DamageInfo;
 import com.megacrit.cardcrawl.cards.blue.LockOn;
@@ -29,40 +27,24 @@ public class Snipe extends AbstractEasyCard {
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        dmg(m, AbstractGameAction.AttackEffect.SLASH_VERTICAL);
         addToBot(new SelectCardsInHandAction(1, cardStrings.EXTENDED_DESCRIPTION[1], l -> {
             for (AbstractCard c : l) {
-                int bonus = c.costForTurn == -1 ? EnergyPanel.totalCount : Math.max(c.costForTurn, 0);
-                Wiz.att(new ApplyPowerAction(m, p, new ExposedPower(m, magicNumber + bonus)));
-                Wiz.att(new DamageAction(m, new DamageInfo(p, damage, damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL, true));
-                Wiz.att(new DiscardSpecificCardAction(c, Wiz.adp().hand));
+                int bonus = c.costForTurn == -1 ? EnergyPanel.getCurrentEnergy() : Math.max(c.costForTurn, 0);
+                Wiz.att(new DamageAction(m, new DamageInfo(p, damage + (bonus*magicNumber), damageTypeForTurn), AbstractGameAction.AttackEffect.SLASH_HORIZONTAL, true));
+                Wiz.att(new ExhaustSpecificCardAction(c, Wiz.adp().hand));
             }
         }));
     }
 
     @Override
-    public boolean canUse(AbstractPlayer p, AbstractMonster m) {
-        boolean canUse = super.canUse(p, m);
-        if (canUse) {
-            for (AbstractCard c : p.hand.group) {
-                if (c != this) {
-                    return true;
-                }
-            }
-        }
-        this.cantUseMessage = cardStrings.EXTENDED_DESCRIPTION[0];
-        return false;
-    }
-
-    @Override
     public void upp() {
-        upgradeDamage(3);
+        //upgradeDamage(3);
         upgradeMagicNumber(2);
     }
 
     @Override
     public CardArtRoller.ReskinInfo reskinInfo(String ID) {
-        return new CardArtRoller.ReskinInfo(ID, 0.5f, 0.5f, 0.2f, 0.5f, false);
+        return new CardArtRoller.ReskinInfo(ID, 0.5f, 0.5f, 0.25f, 0.5f, false);
     }
 
     @Override
