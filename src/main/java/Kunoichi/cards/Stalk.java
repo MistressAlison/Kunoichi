50362a6f04e3c8b83b794b7182eeede15b3e74b5
Alison Turner <75217888+MistressAlison@users.noreply.github.com>
2023-04-15 00:09:16 -0300

    Stalk no longer applies Weak

--- src/main/java/Kunoichi/cards/Stalk.java
+++ src/main/java/Kunoichi/cards/Stalk.java
@@ -4,13 +4,9 @@ import Kunoichi.cards.abstracts.AbstractEasyCard;
 import Kunoichi.powers.ExposedPower;
 import Kunoichi.util.CardArtRoller;
 import Kunoichi.util.Wiz;
-import com.megacrit.cardcrawl.cards.red.SeeingRed;
 import com.megacrit.cardcrawl.cards.tempCards.Safety;
 import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
-import com.megacrit.cardcrawl.powers.VulnerablePower;
-import com.megacrit.cardcrawl.powers.WeakPower;
-import com.megacrit.cardcrawl.powers.watcher.FreeAttackPower;
 
 import static Kunoichi.KunoichiMod.makeID;
 
@@ -19,21 +15,18 @@ public class Stalk extends AbstractEasyCard {
 
     public Stalk() {
         super(ID, 1, CardType.SKILL, CardRarity.COMMON, CardTarget.ENEMY);
-        baseMagicNumber = magicNumber = 1;
-        baseSecondMagic = secondMagic = 4;
+        baseMagicNumber = magicNumber = 4;
         exhaust = true;
     }
 
     @Override
     public void use(AbstractPlayer p, AbstractMonster m) {
-        Wiz.applyToEnemy(m, new WeakPower(m, magicNumber, false));
-        Wiz.applyToEnemy(m, new ExposedPower(m, secondMagic));
+        Wiz.applyToEnemy(m, new ExposedPower(m, magicNumber));
     }
 
     @Override
     public void upp() {
-        upgradeMagicNumber(1);
-        upgradeSecondMagic(1);
+        upgradeMagicNumber(2);
     }
 
     @Override
